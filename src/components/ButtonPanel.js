49e93c6fbe48219aa4a7d50914c43067f7cb78b1
Zappat0n <albarros1973@gmail.com>
2021-03-16 15:26:30 +0100

    Add props with clickHandler to ButtonPanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,37 +1,46 @@
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-const ButtonPanel = () => (
-  <>
-    <div className="group-1">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="/" />
-    </div>
-    <div className="group-2">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
-      <Button name="X" />
-    </div>
-    <div className="group-3">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
-      <Button name="-" />
-    </div>
-    <div className="group-4">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
-      <Button name="+" />
-    </div>
-    <div className="group-5">
-      <Button name="0" />
-      <Button name="." />
-      <Button name="=" />
-    </div>
-  </>
-);
+const ButtonPanel = ({ clickHandler }) => {
+  const createButton = name => (<Button name={name} onClick={clickHandler} />);
+
+  return (
+    <>
+      <div className="group-1">
+        {createButton('AC')}
+        {createButton('+/-')}
+        {createButton('%')}
+        {createButton('/')}
+      </div>
+      <div className="group-2">
+        {createButton('7')}
+        {createButton('8')}
+        {createButton('9')}
+        {createButton('X')}
+      </div>
+      <div className="group-3">
+        {createButton('4')}
+        {createButton('5')}
+        {createButton('6')}
+        {createButton('-')}
+      </div>
+      <div className="group-4">
+        {createButton('1')}
+        {createButton('2')}
+        {createButton('3')}
+        {createButton('+')}
+      </div>
+      <div className="group-5">
+        {createButton('0')}
+        {createButton('.')}
+        {createButton('=')}
+      </div>
+    </>
+  );
+};
+
+ButtonPanel.propTypes = {
+  clickHandler: PropTypes.func.isRequired,
+};
 
 export default ButtonPanel;
