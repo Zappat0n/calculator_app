007e72601da37ecdabea9d7b9015dc0965ae9987
Zappat0n <albarros1973@gmail.com>
2021-03-16 14:36:46 +0100

    Change code format in calculate.js

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -18,13 +18,39 @@ const calculate = (calculatorData, buttonName) => {
   };
 
   switch (buttonName) {
-    case 'AC': updateData('0', '0', null); break;
-    case '+/-': updateData(total, Big(next).times(-1), operation); break;
-    case '+': case '-': case '*': case '/': case '%':
-      updateResult(operate(total, next, operation)); break;
-    case '0': case '1': case '2': case '3': case '4': case '5': case '6': case '7': case '8': case '9': case '.': updateData(total, `${next}${buttonName}`, operation); break;
-    case '=': updateResult(operate(total, next, operation)); break;
-    default: return null;
+    case 'AC':
+      newData.total = '';
+      newData.next = '';
+      newData.operation = null;
+      break;
+    case '+/-':
+      updateData(total, Big(next).times(-1), operation);
+      break;
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+    case '%':
+      updateResult(operate(total, next, operation));
+      break;
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '.':
+      updateData(total, `${next}${buttonName}`, operation);
+      break;
+    case '=':
+      updateResult(operate(total, next, operation));
+      break;
+    default:
+      return null;
   }
 
   return newData;
