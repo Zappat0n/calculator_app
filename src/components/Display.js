17dc64b7781f272ba1f42b362898a933c25ca504
Zappat0n <albarros1973@gmail.com>
2021-03-16 10:02:06 +0100

    Add basic Display component

---
+++ src/components/Display.js
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const Display = props => {
+  const { result } = props;
+
+  return (
+    <p>
+      { result }
+    </p>
+  );
+};
+
+Display.propTypes = {
+  result: PropTypes.string.isRequired,
+};
+
+export default Display;
