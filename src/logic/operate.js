7724feb9591f6e6ed1fda91f90810fb37c2d75ac
Zappat0n <albarros1973@gmail.com>
2021-03-16 15:32:01 +0100

    Cast numberTwo to Big in operate.js

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -2,13 +2,14 @@ import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
   const num1 = new Big(numberOne);
+  const num2 = new Big(numberTwo);
 
   switch (operation) {
-    case '+': return num1.plus(numberTwo).toFixed();
-    case '-': return num1.minus(numberTwo).toFixed();
-    case '*': return num1.times(numberTwo).toFixed();
-    case '/': return num1.div(numberTwo).toFixed();
-    case '%': return 100 * num1.div(numberTwo).toFixed();
+    case '+': return num1.plus(num2).toFixed();
+    case '-': return num1.minus(num2).toFixed();
+    case '*': return num1.times(num2).toFixed();
+    case '/': return num1.div(num2).toFixed();
+    case '%': return 100 * num1.div(num2).toFixed();
     default: return null;
   }
 };
