efa48b656235c094cf7dba335aace49cd0049ffa
Zappat0n <albarros1973@gmail.com>
2021-03-16 15:25:51 +0100

    Change App Object to Class

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-const App = () => (
-  <>
-    {<Display />}
-    {<ButtonPanel />}
-  </>
-);
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      // total: null,
+      next: null,
+      // operation: null,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { next } = this.state;
+    console.log(buttonName, next);
+  }
+
+  render() {
+    const { next } = this.state;
+    return (
+      <>
+        {<Display result={next} />}
+        {<ButtonPanel clickHandler={this.handleClick()} />}
+      </>
+    );
+  }
+}
 
 export default App;
