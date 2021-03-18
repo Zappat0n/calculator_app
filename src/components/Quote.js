16bac61fb51f7abb637d9d1fdc463b09be62fae1
Zappat0n <albarros1973@gmail.com>
2021-03-18 12:24:03 +0100

    Create Routes

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from './NavBar';
 
-const Home = () => (
+const Quote = () => (
   <>
     <NavBar message="" />
     <h4>
@@ -11,4 +11,4 @@ const Home = () => (
   </>
 );
 
-export default Home;
+export default Quote;
