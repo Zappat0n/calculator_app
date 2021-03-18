aedc96579a57d00092d368731d374f847b633996
Zappat0n <albarros1973@gmail.com>
2021-03-18 12:21:35 +0100

    Add Quote component

---
+++ src/components/Quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+
+const Home = () => (
+  <>
+    <NavBar message="" />
+    <h4>
+      Mathematics is not about numbers, equations, computations,
+      or algorithms: it is about understanding. –William Paul Thurston
+    </h4>
+  </>
+);
+
+export default Home;
